a1741b235b9a7f467b9608ffac117d462e6b2129
suraj tomar <surajtomar750@gmail.com>
2018-09-23 20:07:24 +0530

    now routing is working

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-const express = require('express');
-const mongoose = require('mongoose')
-const app = express();
+var express = require('express');
+var app = express();
+// const mongoose = require('mongoose')
+
 const routes = require('./route/routes');
-url="mongodb://localhost:27017/mybase"
-mongoose.connect(url);
-const dbConnection = mongoose.connection;
-dbConnection.on('open',(err)=>{
-    if(err){ throw err;}
-    console.log("connection successful with mongodb")
-});
+// url="mongodb://localhost:27017/mybase"
+// mongoose.connect(url);
+// const dbConnection = mongoose.connection;
+// dbConnection.on('open',(err)=>{
+//     if(err){ throw err;}
+//     console.log("connection successful with mongodb")
+// });
 
-dbConnection.on('error', console.error.bind(console, 'connection error:'));
+// dbConnection.on('error', console.error.bind(console, 'connection error:'));
 
-app.use(express.json);
+// 
 
+// app.get('/',(req,res)=>{
+//     res.send("hello now routing is working");
+// })
 app.use('/',routes);
 
 
--- route/routes.js
+++ route/routes.js
@@ -1,33 +1,33 @@
 const express = require('express')
 const pController = require('../controller/productController')
-const router = express.Router();
+const mRouter = express.Router();
 
 
-router.get('/',(req,res)=>{
+mRouter.get('/',(req,res)=>{
     res.send("nothing is here");
     
     });
-router.get('/product',(req,res)=>{
+mRouter.get('/product',(req,res)=>{
     res.send("this is product");
     });
 
-router.get('/login',()=>{
-
+mRouter.get('/login',(req,res)=>{
+        res.send("login is working");
     });
 
 
-router.get('/products',(req,res)=>{
+mRouter.get('/products',(req,res)=>{
     console.log("products requested")
-    pController.getProducts(req,res);
+   // pController.getProducts(req,res);
     });
     
-router.get('/product/:index',(req,res)=>{
+mRouter.get('/product/:index',(req,res)=>{
     
     res.send(products[req.params.index]);
     
     });
     
-    router.post('/login',(req,res)=>{
+    mRouter.post('/login',(req,res)=>{
      for(var i=0;i<users.length;i++){
          if(users[i].userid == req.body.userid){
              if(users[i].pass == req.body.password){
@@ -41,7 +41,7 @@ router.get('/product/:index',(req,res)=>{
      }
     });
     
-    router.get('/Status',(req,res)=>{
+    mRouter.get('/Status',(req,res)=>{
         
         res.status(404).send("this is a custom status code");
         
@@ -49,8 +49,8 @@ router.get('/product/:index',(req,res)=>{
     
  
     
-    router.get(/.*fly$/,(req,res)=>{
+    mRouter.get(/.*fly$/,(req,res)=>{
         res.send(req.url);
     });
 
-module.exports = router;
+module.exports = mRouter;
